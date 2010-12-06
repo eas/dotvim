dd998f90b54812eaf54b7b2e3c87c0454ce17b01
Andrei Elovikov <a.elovikov@gmail.com>
2010-12-07 00:10:22 +0600

    Script for generating systags file was added.

---
+++ systags.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+SYSTAGS_OUT="$HOME/.vim/systags"
+TAGFLIST="/usr/include/*"
+RTAGFLIST="/usr/include/gtk-2.0/ \
+    /usr/include/sys/ \
+    /usr/include/bits/ \
+    /usr/include/glib-2.0/ \
+    /usr/include/mysql/ \
+    /usr/include/net \
+    /usr/include/netinet \
+    /usr/include/cairo"
+
+CTAGS_FLAGS="--c++-kinds=+p --fields=+iaS --extra=+q"
+
+[[ -x /usr/bin/ctags ]] && \
+    /usr/bin/ctags --verbose $CTAGS_FLAGS -f $SYSTAGS_OUT $TAGFLIST -R $RTAGFLIST
+ls -lh $SYSTAGS_OUT
