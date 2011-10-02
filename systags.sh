958907526952ac738c4bc135da75aab8a4c1f3f2
Andrei Elovikov <a.elovikov@gmail.com>
2011-10-02 15:47:45 +0700

    Add Qt include directories to systags.sh

--- systags.sh
+++ systags.sh
@@ -9,7 +9,8 @@ RTAGFLIST="/usr/include/gtk-2.0/ \
     /usr/include/mysql/ \
     /usr/include/net \
     /usr/include/netinet \
-    /usr/include/cairo"
+    /usr/include/cairo \
+    /usr/include/qt4"
 
 CTAGS_FLAGS="--c++-kinds=+p --fields=+iaS --extra=+q"
 
